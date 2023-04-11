d21db84b874badcfc6365f483fc38dd65eee1cfd
Malay Sarkar <malay.cse@gmail.com>
2023-04-12 00:58:45 +0530

    header with logo implemeted

---
+++ src/app/my-mat-components/my-mat-components.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatButtonModule} from '@angular/material/button';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatTableModule} from '@angular/material/table';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatBadgeModule,
+    MatCardModule,
+    MatInputModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatToolbarModule
+  ]
+})
+export class MyMatComponentsModule { }
