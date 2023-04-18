f7cde6bf4a695372b617d896f82fa6390aab9fe4
Malay Sarkar <malay.cse@gmail.com>
2023-04-18 13:48:22 +0530

    Profile Component Implemented

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginServiceService } from '../services/login-service.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent{
+
+  user:any=this.login.getUser();
+  
+  constructor(private login:LoginServiceService){}
 
 }
